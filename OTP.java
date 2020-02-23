ab386b8828caf1644995e16cb0faa7220860f9e1
felizgaming1 <52193806+felizgaming1@users.noreply.github.com>
2020-02-23 19:39:36 +0530

    Update OTP.java

--- OTP.java
+++ OTP.java
@@ -3,12 +3,11 @@ import java.util.Random;
 public class OTP {
     static char[] sendotp(int length){
         System.out.println("your OTP");
-        String numbers="0123456789";
-        Random r=new Random();
+        String numbers="0123456789";    //this is set the limit to use only this set of words
+        Random r=new Random();          //this command create an object of ranndom variable
         char otp[]=new char[length];
         for (int i=0;i<length;i++){
-            otp[i]=numbers.charAt(r.nextInt(numbers.length()));
-
+            otp[i]=numbers.charAt(r.nextInt(numbers.length()));  //this command is excuted for the 4 time 
         }
         return otp;
     }
